502f317dce9c61f01c4b57ef7622a7e68f341e85
G.M <47867339+GMLJ@users.noreply.github.com>
2021-07-21 22:27:26 +0200

    shopping list

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "play on the switch", key: "3" },
+    { text: "Hi Love ! Ere min inibla?", key: "1" },
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 2) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert(
+        "OOPS!",
+        "Too short ! This can't be a word ma belle, doesn't make sense ! Am I supposed to guess what it is?",
+        [
+          {
+            text: "Chef Oui Chef !",
+            onPress: () => console.log("Alert closed"),
+          },
+        ]
+      );
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Header />
-      <View style={styles.container}>
-        {/* to do form */}
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
